bfb8045a420df251c842d6e784311e7c4cbd268e
co1dmountain <co1dmountain@xq.com>
2023-04-21 19:40:21 +0800

    将不买票、

---
+++ prj/ip_inst.v
@@ -0,0 +1,5 @@
+ip	ip_inst (
+	.address ( address_sig ),
+	.clock ( clock_sig ),
+	.q ( q_sig )
+	);
--- src/vga_colorbar_display.v
+++ src/vga_colorbar_display.v
@@ -8,7 +8,8 @@ module vga_colorbar_display(
 		// output	reg [15:0]			pixel_data	
 		output	reg [29:0]			pixel_data
 );
-	
+
+/*	first verision
 	//// define ////
 	parameter  H_DISP = 10'd640; //分辨率—行
 	parameter  V_DISP = 10'd480; //分辨率—列
@@ -47,8 +48,69 @@ module vga_colorbar_display(
 			end
 		end
 	end
-
-
+*/	
+	
+	//// define ////
+	wire [13:0] rom_addr;
+	wire [15:0] rom_data; 
+	
+	reg  [13:0] rom_addr_cnt;
+	wire 			region_valid;  // valid region
+	
+	//// main code ////
+	// rom_addr_cnt
+	always @(posedge driver_clk or negedge sys_rst_n) begin
+		 if(!sys_rst_n) begin
+			   rom_addr_cnt <= 'd0;
+		 end
+		 else if(region_valid) begin
+				if(rom_addr_cnt == 'd9599) begin
+					rom_addr_cnt <= 'd0;
+				end
+				else begin
+					rom_addr_cnt <= rom_addr_cnt + 1'b1;
+				end
+		 end
+		 else begin
+				rom_addr_cnt <= rom_addr_cnt;
+		 end
+	end
+	
+	// region_valid
+	assign region_valid = ((pixel_xpos >= 'd100 && pixel_xpos < 'd340) 
+									&& (pixel_ypos >= 'd50 && pixel_ypos < 'd129)) ? 
+									1'b1 : 1'b0;
+	
+	// rom_addr
+	assign rom_addr = rom_addr_cnt;
+	
+	// pixel_data
+	always @(posedge driver_clk or negedge sys_rst_n) begin
+		if(!sys_rst_n) begin
+			pixel_data <= 'd0;
+		end
+		else if(region_valid) begin
+			pixel_data <= {{5'b0, rom_data[11], rom_data[12], rom_data[13], rom_data[14], rom_data[15]},
+								{4'b0, rom_data[5], rom_data[6], rom_data[7], rom_data[8], rom_data[9], rom_data[10]},
+								{5'b0, rom_data[0], rom_data[1], rom_data[2], rom_data[3], rom_data[4]}
+							  };
+		end
+		else begin
+			pixel_data <= 'd0;
+		end
+	end
+	
+	
+	
+	
+	
+	
+	// rom ip
+	ip rom0(
+		.address		(rom_addr			),    // addr
+		.clock		(driver_clk			),    // clk
+		.q				(rom_data			)     // data out
+	);
 
 
 
